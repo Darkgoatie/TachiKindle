a268fbdc0ded6dc55877e3be19cd84f8958c4798
Darkgoatie <81323822+Darkgoatie@users.noreply.github.com>
2026-09-22 15:56:20 +0300

    Remove standalone native binary/Scriptlet app, KOReader plugin only

--- src/ui_chapters.h
+++
@@ -1,9 +0,0 @@
-#ifndef CI_UI_CHAPTERS_H
-#define CI_UI_CHAPTERS_H
-#include "app.h"
-#include "input.h"
-
-void ui_chapters_draw(app_t *app);
-void ui_chapters_handle(app_t *app, const ci_event_t *ev);
-
-#endif
--- src/ui_library.h
+++
@@ -1,9 +0,0 @@
-#ifndef CI_UI_LIBRARY_H
-#define CI_UI_LIBRARY_H
-#include "app.h"
-#include "input.h"
-
-void ui_library_draw(app_t *app);
-void ui_library_handle(app_t *app, const ci_event_t *ev);
-
-#endif
